5c14713fef257a7ae2d424b62e91079db4b80553
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-30 02:40:54 +0100

    feat: add zx9Speaker component

--- src/common/Products/Products.tsx
+++ src/common/Products/Products.tsx
@@ -1,28 +1,47 @@
 import React from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./Products.module.scss";
+import zx9Desktop from "../../assets/home/desktop/image-speaker-zx9.png";
+import zx9Tablet from "../../assets/home/tablet/image-speaker-zx9.png";
+import zx9Mobile from "../../assets/home/mobile/image-speaker-zx9.png";
 
 export default function Products() {
   return (
     <Box className={styles.products}>
-      <Box className={styles.products__first}>
-        <Box>
-          <StaticImage
-            src="../../assets/home/desktop/image-speaker-zx9.png"
-            alt=""
-          />
+      <Box className={styles.products__zX9Speaker}>
+        <Box className={styles.products__zX9Speaker_imageContainer}>
+          {/* <StaticImage
+            src={"../../assets/home/desktop/image-speaker-zx9.png"}
+            alt="ZX9 Speaker"
+            className={styles.products__zX9Speaker_image}
+          /> */}
+          <picture>
+            <source media="(min-width:1024px)" srcSet={zx9Desktop} />
+            <source media="(min-width:768px)" srcSet={zx9Tablet} />
+            <img
+              src={zx9Mobile}
+              alt="ZX9 Speaker"
+              className={styles.products__zX9Speaker_image}
+            />
+          </picture>
         </Box>
-        <Box>
-          <Typography>ZX9 speaker</Typography>
-          <Typography>
+        <Box className={styles.products__zX9Speaker_content}>
+          <Typography className={styles.products__zX9Speaker_title}>
+            ZX9 speaker
+          </Typography>
+          <Typography className={styles.products__zX9Speaker_description}>
             Upgrade to premium speakers that are phenomenally built to deliver
             truly remarkable sound.
           </Typography>
-          <Button variant="text">See product</Button>
+          <Button
+            variant="text"
+            className={`${styles.button} ${styles.products__zX9Speaker_button}`}
+          >
+            See product
+          </Button>
         </Box>
       </Box>
-      {/* <Box className={styles.products__second}> */}
       <Box className={`${styles.products__zX7Speaker}`}>
         <Typography
           className={`
@@ -41,9 +60,8 @@ export default function Products() {
         >
           See product
         </Button>
-        {/* </Box> */}
       </Box>
-      <Box className={styles.products__third}>
+      <Box className={styles.products__yx1Earphones}>
         <Box className={styles.products__yx1ImageContainer}>
           <StaticImage
             src="../../assets/home/desktop/image-earphones-yx1.jpg"
