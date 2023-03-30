a41e8e95d8c83514dd3355028bf37851d820e811
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-30 13:20:45 +0100

    fix: reposition the hero background image with respect to the design

--- src/common/Layout/Header/Header.tsx
+++ src/common/Layout/Header/Header.tsx
@@ -3,31 +3,49 @@ import { Box, Typography } from "@mui/material";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import MenuIcon from "@mui/icons-material/Menu";
+import * as styles from "./Header.module.scss";
 
 export default function Header() {
   return (
-    <Box component="header">
+    <Box component="header" className={styles.header}>
       <Box component="nav">
-        <MenuIcon />
-        <StaticImage src="../assets/shared/desktop/logo.svg" alt="" />
-        <Box component="ul">
-          <Typography component="li">
-            <Link to="/">Home</Link>
+        <MenuIcon className={styles.header__hamburger} />
+        <Box>
+          <StaticImage
+            className={styles.header__logo}
+            src="../../../assets/shared/desktop/logo.svg"
+            alt="logo"
+          />
+        </Box>
+        <Box component="ul" className={styles.header__menu}>
+          <Typography component="li" className={styles.header__menuItem}>
+            <Link to="/" className={styles.header__menuItem_text}>
+              Home
+            </Link>
           </Typography>
-          <Typography component="li">
-            <Link to="/headphones">Headphones</Link>
+          <Typography component="li" className={styles.header__menuItem}>
+            <Link to="/headphones" className={styles.header__menuItem_text}>
+              Headphones
+            </Link>
           </Typography>
-          <Typography component="li">
-            <Link to="/speakers">Speakers</Link>
+          <Typography component="li" className={styles.header__menuItem}>
+            <Link to="/speakers" className={styles.header__menuItem_text}>
+              Speakers
+            </Link>
           </Typography>
-          <Typography component="li">
-            <Link to="/earphones">Earphones</Link>
+          <Typography component="li" className={styles.header__menuItem}>
+            <Link to="/earphones" className={styles.header__menuItem_text}>
+              Earphones
+            </Link>
           </Typography>
         </Box>
-        <StaticImage
-          src="../../../assets/shared/desktop/icon-cart.svg"
-          alt="cart icon"
-        />
+        <Box>
+          <StaticImage
+            src="../../../assets/shared/desktop/icon-cart.svg"
+            alt="cart icon"
+            className={styles.header__cart}
+          />
+        </Box>
       </Box>
     </Box>
   );
