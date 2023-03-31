59b63d4ab7603f4a7261769a2c957669ddcb5514
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-03-31 17:38:39 +0100

    feat: create a gallery for each good

--- src/pages/category/headphones/xx99Two.tsx
+++ src/pages/category/headphones/xx99Two.tsx
@@ -1,12 +1,45 @@
 import * as React from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { ProductCategoryLayout, RelatedProducts } from "../../../common";
+import * as styles from "../Category.module.scss";
+import gal1D from "../../../assets/product-xx99-mark-two-headphones/desktop/image-gallery-1.jpg";
+import gal2D from "../../../assets/product-xx99-mark-two-headphones/desktop/image-gallery-2.jpg";
+import gal3D from "../../../assets/product-xx99-mark-two-headphones/desktop/image-gallery-3.jpg";
+import gal1T from "../../../assets/product-xx99-mark-two-headphones/tablet/image-gallery-1.jpg";
+import gal2T from "../../../assets/product-xx99-mark-two-headphones/tablet/image-gallery-2.jpg";
+import gal3T from "../../../assets/product-xx99-mark-two-headphones/tablet/image-gallery-3.jpg";
+import gal1M from "../../../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg";
+import gal2M from "../../../assets/product-xx99-mark-two-headphones/mobile/image-gallery-2.jpg";
+import gal3M from "../../../assets/product-xx99-mark-two-headphones/mobile/image-gallery-3.jpg";
 
 export default function XX99One() {
   return (
     <ProductCategoryLayout>
       <Typography>Go back</Typography>
       <h1>XX99 Two</h1>
+      <Box className={styles.gallery}>
+        <Box className={styles.gallery__pic1}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal1D} />
+            <source media="(min-width:768px)" srcSet={gal1T} />
+            <img src={gal1M} alt="" />
+          </picture>
+        </Box>
+        <Box className={styles.gallery__pic2}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal2D} />
+            <source media="(min-width:768px)" srcSet={gal2T} />
+            <img src={gal2M} alt="" />
+          </picture>
+        </Box>
+        <Box className={styles.gallery__pic3}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal3D} />
+            <source media="(min-width:768px)" srcSet={gal3T} />
+            <img src={gal3M} alt="" />
+          </picture>
+        </Box>
+      </Box>
       <RelatedProducts />
     </ProductCategoryLayout>
   );
--- src/pages/category/speakers/zx9.tsx
+++ src/pages/category/speakers/zx9.tsx
@@ -1,12 +1,46 @@
 import { ProductCategoryLayout, RelatedProducts } from "../../../common";
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import gal1D from "../../../assets/product-zx9-speaker/desktop/image-gallery-1.jpg";
+import gal2D from "../../../assets/product-zx9-speaker/desktop/image-gallery-2.jpg";
+import gal3D from "../../../assets/product-zx9-speaker/desktop/image-gallery-3.jpg";
+import gal1T from "../../../assets/product-zx9-speaker/tablet/image-gallery-1.jpg";
+import gal2T from "../../../assets/product-zx9-speaker/tablet/image-gallery-2.jpg";
+import gal3T from "../../../assets/product-zx9-speaker/tablet/image-gallery-3.jpg";
+import gal1M from "../../../assets/product-zx9-speaker/mobile/image-gallery-1.jpg";
+import gal2M from "../../../assets/product-zx9-speaker/mobile/image-gallery-2.jpg";
+import gal3M from "../../../assets/product-zx9-speaker/mobile/image-gallery-3.jpg";
+
+import * as styles from "../Category.module.scss";
 
 export default function Xx99One() {
   return (
     <ProductCategoryLayout>
       <Typography>Go back</Typography>
       <h1>ZX9 Speakers</h1>
+      <Box className={styles.gallery}>
+        <Box className={styles.gallery__pic1}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal1D} />
+            <source media="(min-width:768px)" srcSet={gal1T} />
+            <img src={gal1M} alt="" />
+          </picture>
+        </Box>
+        <Box className={styles.gallery__pic2}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal2D} />
+            <source media="(min-width:768px)" srcSet={gal2T} />
+            <img src={gal2M} alt="" />
+          </picture>
+        </Box>
+        <Box className={styles.gallery__pic3}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={gal3D} />
+            <source media="(min-width:768px)" srcSet={gal3T} />
+            <img src={gal3M} alt="" />
+          </picture>
+        </Box>
+      </Box>
       <RelatedProducts />
     </ProductCategoryLayout>
   );
